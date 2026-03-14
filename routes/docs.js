40465a6e73a89a345562d54b93dfcf714fcd8a0f
Eshani <eshanib@karunya.edu.in>
2026-03-14 13:45:00 +0530

    Fixed an bug in docs.js to the project

--- routes/docs.js
+++ routes/docs.js
@@ -1,4 +1,4 @@
-# Hello World -> Akash my love
+// Hello World -> Akash my love
 const express  = require('express');
 const router   = express.Router();
 const { chat } = require('../utils/grokClient');
